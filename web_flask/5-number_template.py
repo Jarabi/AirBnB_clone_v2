ff502235a790d7484b2c67b33bc2805787184d0e
Alex Jarabi <jarabialex@gmail.com>
2023-09-22 23:15:08 +0000

    Including number template

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application """
+from flask import Flask, render_template
+from markupsafe import escape
+
+app = Flask(__name__)
+
+# Disable strict slashes for all routes
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    """ Displays: Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Displays: HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def show_text(text):
+    """
+    Displays: C <text>
+    Args:
+        text: required string
+    """
+
+    # Replace underscores with spaces
+    text = text.replace('_', ' ')
+
+    return f"C {escape(text)}"
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def default_value(text="is cool"):
+    """
+    Displays: Python <text>
+    Args:
+        text: Optional, default value "is cool"
+    """
+    text = text.replace('_', ' ')
+
+    return f"Python {escape(text)}"
+
+
+@app.route('/number/<int:n>')
+def integer_value(n):
+    """
+    Displays: <int:n> is a number>
+    Args:
+        n: must be integer value
+    """
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>')
+def show_template(n):
+    """
+    Displays: HTML template
+    Args:
+        n: must be integer value
+    """
+    return render_template('5-number.html', number=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
