245118d6c57422c366a1d49061d27b55f61cc84e
Alex Jarabi <jarabialex@gmail.com>
2023-09-24 21:55:02 +0000

    List states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" A script that starts a Flask web application """
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+# Disable strict slashes for all routes
+app.url_map.strict_slashes = False
+
+@app.route('/states_list')
+def list_states():
+    """ Gets all states in database """
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+@app.teardown_appcontext
+def tear_down(arg=None):
+    """ Remove SQLAlchemy session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
